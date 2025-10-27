70600e5c0bc84722e0d1abea61ca149093083dc3
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2025-10-27 07:55:16 +0200

    Initial commit

---
+++ GUIExample/InterestCalculator.java
@@ -0,0 +1,7 @@
+
+public class InterestCalculator {
+	double calculate(double principal, double rate, double time) {
+		return (principal * rate * time) / 100;
+	}
+
+}
---
+++ HelloWorld/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, Mulenga Weelcome to the  wonderful world of JAVA programming!");
+	}
+
+}
